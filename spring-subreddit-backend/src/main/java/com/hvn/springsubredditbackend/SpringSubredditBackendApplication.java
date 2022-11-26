09c0c056817011caa8f499de7e02b516163bc86c
nguyenhv <nguyenhv236@gmail.com>
2022-11-26 20:04:31 +0700

    #commit code

---
+++ spring-subreddit-backend/src/main/java/com/hvn/springsubredditbackend/SpringSubredditBackendApplication.java
@@ -0,0 +1,13 @@
+package com.hvn.springsubredditbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSubredditBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSubredditBackendApplication.class, args);
+	}
+
+}
